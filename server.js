6aedc399ca78635a26fba53aff2159bbce804833
barbaragncarvalho <barbarakawanyg@gmail.com>
2025-07-14 15:53:07 -0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
+const fs = require('fs');
+const lanches = JSON.parse(fs.readFileSync(path.join(__dirname, 'public', 'data', 'lanches.json'), 'utf8'));
+
+app.use(express.static(path.join(__dirname, 'public'))); //disponibiliza arquivo da pasta public para serem acessados nas rotas criadas
+app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.urlencoded({ extended: true })); //
+
+app.get('/', (requisicao, resposta) => { //caso seja feita requisição get, retorna essa resposta
+    resposta.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/sugestao', (requisicao, resposta) => {
+    const { nome, ingredientes } = requisicao.query;
+    resposta.send(`
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agradecimento</title>
+</head>
+<body>
+    <header>
+        <img src="../public/images/hamburger.svg" alt="Logo com desenho de Hambúrguer da DevBurger">
+        <h6></h6><a href="/contato">Contato</a></h6>
+    </header>
+    <h1>Obrigado, ${nome}!</h1>
+        <p>Recebemos sua sugestão de hambúrguer com os seguintes ingredientes:</p>
+        <p>${ingredientes}</p>
+      <a href="/">Voltar para a página inicial</a>
+      <footer>
+        <p>Todos os direitos reservados à DevBurger</p>
+        <p>©2025</p>
+    </footer>
+</body>
+</html>
+        `);
+});
+
+app.get('/not-found', (requisicao, resposta) => {
+    resposta.status(404).sendFile(path.join(__dirname, '404.html'));
+});
+
+app.get('/contato', (requisicao, resposta) => {
+    resposta.sendFile(path.join(__dirname, 'views', 'contato.html'));
+});
+
+app.post('/contato', (requisicao, resposta) => {
+    const { nome, email, assunto, mensagem } = requisicao.body;
+
+    if (!nome || !email || !assunto || !mensagem) {
+        return resposta.status(400).sendFile(path.join(__dirname, '404.html'));
+    }
+
+    resposta.send(`
+        <!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contato recebido</title>
+</head>
+<body>
+    <header>
+        <img src="../public/images/hamburger.svg" alt="Logo com desenho de Hambúrguer da DevBurger">
+        <h6></h6><a href="/contato">Contato</a></h6>
+    </header>
+        <h1>Contato recebido! Obrigado, ${nome}.</h1>
+        <p><strong>Seu e-mail: </strong>${email}</p>
+        <p><strong>Assunto:</strong> ${assunto}</p>
+        <p><strong>Mensagem:</strong> ${mensagem}</p>
+        <a href="/contato">Voltar ao formulário de contato</a>
+    <footer>
+        <p>Todos os direitos reservados à DevBurger</p>
+        <p>©2025</p>
+    </footer>
+</body>
+</html>
+        `);
+});
+
+app.get('/api/lanches', (requisicao, resposta) => {
+    resposta.status(200).json(lanches);
+});
+
+app.listen(port, () => { //indica onde o servidor roda
+    console.log(`Servidor da DevBurger rodando em http://localhost:${port}`);
+});
